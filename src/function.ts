223b9021971f3bcad32a2c96484c677179784fe5
un-mec-paumer <antoine.ciric7@etu.univ-lorraine.fr>
2023-10-16 09:16:17 +0300

    moode de merde a cause de l'RSA et sinon c'st un changement de site car l'autre ne marchais plus

--- src/function.ts
+++ src/function.ts
@@ -5,22 +5,12 @@ type Manga = {name:string, chapitre:number, pages:boolean, discordUsers:string[]
 
 async function finder(manga:Manga): Promise<boolean> {
     let url: string = "";
-    let url2: string = "";
-    if(manga.pages){
-        url = "https://fr-scan.cc/manga/" + manga.name + "/chapitre-" + manga.chapitre + "-vf/p/1000000";
-        manga.chapitre++;
-        url2 = "https://fr-scan.cc/manga/" + manga.name + "/chapitre-" + manga.chapitre + "-vf/";
-    }
-    else{
-        url = "https://fr-scan.cc/manga/" + manga.name + "/chapitre-" + manga.chapitre + "-vf/";
-        manga.chapitre++;
-        url2 = "https://fr-scan.cc/manga/" + manga.name + "/chapitre-" + manga.chapitre + "-vf/";
-    }
+    // let url2: string = "";
+    url = "https://scansmangas.me/scan-" + manga.name + "-" + manga.chapitre + "/";
 
     // console.log(url);
     // console.log(url2);
 
-    manga.chapitre--;
 
     // console.log(manga.chapitre);
 
@@ -35,8 +25,8 @@ async function finder(manga:Manga): Promise<boolean> {
         //manga.chapitre++;
         const text = await response.text();
         // console.log(url);
-        // console.log(text.includes(url2));
-        return text.includes(url2)
+        console.log("https://scansmangas.me/scan-" + manga.name + "-" + (manga.chapitre) + "/", text.includes("https://scansmangas.me/scan-" + manga.name + "-" + (manga.chapitre + 1) + "/"));
+        return text.includes("https://scansmangas.me/scan-" + manga.name + "-" + (manga.chapitre + 1) + "/");
 
         
     } catch (error) {
@@ -49,8 +39,29 @@ async function finder(manga:Manga): Promise<boolean> {
 //     console.log(value);
 // });
 
+// async function finderWebtoon(): Promise<boolean> {
+//     const url = 'https://webtoon.p.rapidapi.com/canvas/search?query=boy%20friend&startIndex=0&pageSize=20&language=fr';
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Key': '0ee492367cmsh3c211d62fd9ead0p1a499cjsnb0b789ef8785',
+//             'X-RapidAPI-Host': 'webtoon.p.rapidapi.com'
+//         }
+//     };
 
-export async function finderAll(client:Client) {
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.text();
+//         console.log(result);
+//     } catch (error) {
+//         console.error(error);
+//     }
+//     return true;
+// }
+
+// finderWebtoon()
+
+export async function finderAll(/*client:Client*/) {
     console.log("finderAll");
     //const userID = "452370867758956554";
 
@@ -59,17 +70,17 @@ export async function finderAll(client:Client) {
             //console.log(value);
             if(value){
                 manga.chapitre++;
-                manga.discordUsers.forEach(userID => {
-                    client.users.fetch(userID).then((user:User) => {
-                        user.send("Nouveau chapitre de " + manga.name + " : " + "https://fr-scan.cc/manga/" + manga.name + "/chapitre-" + manga.chapitre + "-vf/");
-                    });
-                });
+                // manga.discordUsers.forEach(userID => {
+                //     client.users.fetch(userID).then((user:User) => {
+                //         user.send("Nouveau chapitre de " + manga.name + " : " + "https://fr-scan.cc/manga/" + manga.name + "/chapitre-" + manga.chapitre + "-vf/");
+                //     });
+                // });
                 sauvegarder(JSON.stringify(mangas));
             }
         });
     });
 }
-// finderAll();
+finderAll();
 
 export function sauvegarder(data:string/*, path:PathOrFileDescriptor*/):boolean {
     //console.log("data ", data);
