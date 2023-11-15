06db7bea812074271b55317c88a743bc58e9443b
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2023-11-15 23:24:18 +0200

    nike ta mere heroku

--- src/bot.ts
+++ src/bot.ts
@@ -22,7 +22,7 @@ const client = new Client({
         Intents.GuildMessages,
         Intents.DirectMessages,
         Intents.MessageContent,
-        Intents.DirectMessageReactions,
+        Intents.DirectMessageReactions
     ]
 });
 
