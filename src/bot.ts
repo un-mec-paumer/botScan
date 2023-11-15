ea6cd805b512f38163ad2283064e80d488349297
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2023-11-16 00:51:58 +0200

    upload pour heroku le raciste v12

--- src/bot.ts
+++ src/bot.ts
@@ -71,7 +71,7 @@ const app = Express();
 app.use(Express.json());
 
 app.use((req:Request, res:Response, next:NextFunction ) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
