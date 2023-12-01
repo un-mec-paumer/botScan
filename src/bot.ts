cd0c89276368aa2a424476db31c0df7bf61a0d9e
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2023-12-01 15:03:57 +0200

    dellllllllllllllllllllllai

--- src/bot.ts
+++ src/bot.ts
@@ -32,7 +32,7 @@ messageCreate(client);
 
 // console.log(process.env.TOKEN);
 client.login(process.env.TOKEN);
-const interval = setInterval(finderAll, 1000 * 60 * 1, client);
+const interval = setInterval(finderAll, 1000 * 60 * 15, client);
 const interval2 = setInterval(ntm, 1000);
 
 // client.users.fetch("452370867758956554").then((user) => {
