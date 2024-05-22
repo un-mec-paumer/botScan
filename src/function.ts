fb94a64c6f70a9049f7049396be9587938e7a4be
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2024-05-22 16:37:39 +0300

    casse les couille (heroku c'est bien raciste)

--- src/function.ts
+++ src/function.ts
@@ -115,14 +115,14 @@ export async function getCherrioText(url: string) {
     const browser = await puppeteer.launch({
         headless: true,
         args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--no-first-run',
-        '--no-zygote',
-        '--single-process',
-        '--disable-gpu'
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
         ],
         executablePath: process.env.CHROME_BIN
     });
