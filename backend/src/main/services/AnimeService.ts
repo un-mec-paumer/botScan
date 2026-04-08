8da46d96ba8bb4e423ce4484b3f7d74f349711b4
RiY0M <Rigaut_Yoni>
2026-04-08 13:01:42 +0200

    fix: remove workService

--- backend/src/main/services/AnimeService.ts
+++ backend/src/main/services/AnimeService.ts
@@ -1,54 +1,105 @@
 import { PrismaClient } from '@prisma/client';
-import { WorkService } from './WorkService';
 import { AnimeServiceError } from '@errors/AnimeServiceError';
+import { ModelAnime } from '@models/anime';
 
-export class MangaService extends WorkService {
-    constructor(protected readonly prisma: PrismaClient) {
-        super(prisma);
+export class AnimeService {
+    private readonly selection: object = {
+        id: true,
+        name: true,
+        synopsis: true,
+        imgUrl: true,
+        season: true,
+        episode: true,
+        animeSources: {
+            select: {
+                animeSource: {
+                    select: {
+                        id: true,
+                        name: true,
+                    },
+                },
+            },
+        },
+    };
 
-        this.selection = {
-            ...this.selection,
-            // animeSource: true,
-            animeSeason: true,
-            animeEpisode: true,
-        };
+    constructor(protected readonly prisma: PrismaClient) { }
+
+    /**
+     * Récupère un anime.
+     * @param id L'id du anime.
+     */
+    async getAnimeById(id: number): Promise<ModelAnime> {
+        const anime = await this.prisma.Anime.findUnique({
+            select: this.selection,
+            where: { id },
+        });
+
+        if (!anime) {
+            throw new AnimeServiceError('Anime not found.', 404);
+        }
+
+        return new ModelAnime(anime);
     }
 
     /**
      * Récupère un anime.
-     * @param id L'ID de l'anime.
+     * @param id L'id du anime.
      */
-    async getAnimeById(id: number) {
-        return await this.getWorkById(id);
-    }
+    async getAnimeByName(name: string): Promise<ModelAnime> {
+        const anime = await this.prisma.Anime.findFirst({
+            select: this.selection,
+            where: {
+                name: {
+                    contains: name,
+                },
+            },
+        });
 
-    /**
-     * Récupère un anime.
-     * @param id L'ID de l'anime.
-     */
-    async getAnimeByName(name: string) {
-        return await this.getWorkByName(name);
+        if (!anime) {
+            throw new AnimeServiceError('Anime not found.', 404);
+        }
+
+        return new ModelAnime(anime);
     }
 
     /**
      * Récupère les animes
      */
-    async getAnimes() {
-        return await this.prisma.work.findMany();
+    async getAnimes(): Promise<ModelAnime[]> {
+        const animes = await this.prisma.Anime.findMany({ select: this.selection });
+        const animesWithChapter = animes.map(new ModelAnime) as ModelAnime[];
+        return animesWithChapter;
     }
 
     /**
      * Récupère un anime.
-     * @param id L'ID de l'anime.
+     * @param id L'id du anime.
      */
-    /*async addAnime() {
-    const anime = await this.prisma.anime.create({
-    });
+    async addAnime() {
+        const anime = await this.prisma.Anime.create({
+        });
 
-    if (!anime) {
-      throw new AnimeServiceError('Anime already exists.', 409);
+        if (!anime) {
+            throw new AnimeServiceError('Anime already exists.', 409);
+        }
+
+        return anime;
     }
 
-    return anime;
-  }*/
+    async updateChapter(id: number, chapter: string): Promise<ModelAnime> {
+        const anime = await this.prisma.Anime.update({
+            data: {
+                animeChapter: chapter,
+            },
+            where: {
+                id: id,
+            },
+        });
+
+        if (!anime) {
+            throw new AnimeServiceError('Anime not found.', 404);
+        }
+
+        return new ModelAnime(anime);
+    }
 }
--- backend/src/main/services/MangaService.ts
+++ backend/src/main/services/MangaService.ts
@@ -1,39 +1,43 @@
 import { PrismaClient } from '@prisma/client';
-import { WorkService } from './WorkService';
 import { MangaServiceError } from '@errors/MangaServiceError';
 import { ModelManga } from '@models/manga';
 
-export class MangaService extends WorkService {
-    constructor(protected readonly prisma: PrismaClient) {
-        super(prisma);
-
-        this.selection = {
-            ...this.selection,
-            mangaSources: {
-                select: {
-                    mangaSource: {
-                        select: {
-                            id: true,
-                            name: true,
-                        },
+export class MangaService {
+    private readonly selection: object = {
+        id: true,
+        name: true,
+        synopsis: true,
+        imgUrl: true,
+        chapter: true,
+        mangaSources: {
+            select: {
+                mangaSource: {
+                    select: {
+                        id: true,
+                        name: true,
                     },
                 },
             },
-            mangaChapter: true,
-        };
-    }
+        },
+    };
+
+    constructor(protected readonly prisma: PrismaClient) {}
 
     /**
      * Récupère un manga.
      * @param id L'id du manga.
      */
     async getMangaById(id: number): Promise<ModelManga> {
-        const manga = await this.getWorkById(id);
+        const manga = await this.prisma.Manga.findUnique({
+            select: this.selection,
+            where: { id },
+        });
 
         if (!manga) {
             throw new MangaServiceError('Manga not found.', 404);
         }
-        return this.getMangasWithChapter(manga);
+
+        return new ModelManga(manga);
     }
 
     /**
@@ -41,21 +45,28 @@ export class MangaService extends WorkService {
      * @param id L'id du manga.
      */
     async getMangaByName(name: string): Promise<ModelManga> {
-        const manga = await this.getWorkByName(name);
+        const manga = await this.prisma.Manga.findFirst({
+            select: this.selection,
+            where: {
+                name: {
+                    contains: name,
+                },
+            },
+        });
 
         if (!manga) {
             throw new MangaServiceError('Manga not found.', 404);
         }
 
-        return this.getMangasWithChapter(manga);
+        return new ModelManga(manga);
     }
 
     /**
      * Récupère les mangas
      */
     async getMangas(): Promise<ModelManga[]> {
-        const mangas = await this.getWorks();
-        const mangasWithChapter = mangas.map((manga) => this.getMangasWithChapter(manga));
+        const mangas = await this.prisma.Manga.findMany({ select: this.selection });
+        const mangasWithChapter = mangas.map(new ModelManga) as ModelManga[];
         return mangasWithChapter;
     }
 
@@ -63,19 +74,19 @@ export class MangaService extends WorkService {
      * Récupère un manga.
      * @param id L'id du manga.
      */
-    /*async addManga() {
-    const manga = await this.prisma.manga.create({
-    });
+    async addManga() {
+        const manga = await this.prisma.Manga.create({
+        });
 
-    if (!manga) {
-      throw new MangaServiceError('Manga already exists.', 409);
+        if (!manga) {
+        throw new MangaServiceError('Manga already exists.', 409);
+        }
+
+        return manga;
     }
 
-    return manga;
-  }*/
-
     async updateChapter(id: number, chapter: string): Promise<ModelManga> {
-        const manga = await this.prisma.work.update({
+        const manga = await this.prisma.Manga.update({
             data: {
                 mangaChapter: chapter,
             },
@@ -88,16 +99,6 @@ export class MangaService extends WorkService {
             throw new MangaServiceError('Manga not found.', 404);
         }
 
-
-        return this.getMangasWithChapter(manga);
-    }
-
-    
-    
-    private getMangasWithChapter(manga: any): ModelManga { // oui pas de any mais jsp quel type mettre
-        return new ModelManga({
-            ...manga,
-            chapter: manga.mangaChapter,
-        });
+        return new ModelManga(manga);
     }
 }
